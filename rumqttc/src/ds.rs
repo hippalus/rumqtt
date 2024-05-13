b04c10e8451ad60fd2f17143cb8ccac2871348ba
hippalus <habiphakan.isler@gmail.com>
2024-05-13 15:07:16 +0200

    add docs

--- rumqttc/src/ds.rs
+++ rumqttc/src/ds.rs
@@ -1,125 +1,142 @@
+use fixedbitset::FixedBitSet;
+
 use crate::Publish;
 
+/// `OutgoingPublishBucket` is a struct that represents a bucket of outgoing publish messages.
+/// It contains a vector of `Option<Publish>` messages.
 #[derive(Debug, Clone)]
 pub struct OutgoingPublishBucket {
     vec: Vec<Option<Publish>>,
-    len: usize,
 }
 
+/// `OutOfBounds` is a struct that represents an out of bounds error.
+/// It contains a `u16` value that represents the packet identifier that caused the error.
 #[derive(Debug, Copy, Clone)]
 pub struct OutOfBounds(pub u16);
 
 impl OutgoingPublishBucket {
+    /// Creates a new `OutgoingPublishBucket` with a specified limit.
+    /// The limit is used to initialize the vector with `None` values.
     pub fn with_limit(max_pkid: u16) -> Self {
         Self {
             vec: vec![None; max_pkid as usize + 1],
-            len: 0,
         }
     }
 
-    pub fn drain_into<T>(&mut self, vec: &mut Vec<T>, last_puback: usize, map: fn(Publish) -> T) {
-        let (first_half, second_half) = self.split_at_mut(last_puback);
-
-        second_half.iter_mut().chain(first_half).for_each(|opt| {
-            if let Some(v) = opt.take() {
-                vec.push(map(v));
-            }
-        });
-
-        self.clear();
-    }
-
-    fn split_at_mut(&mut self, mid: usize) -> (&mut [Option<Publish>], &mut [Option<Publish>]) {
-        self.vec.split_at_mut(mid)
-    }
-
-    /// Empties the vec.
-    pub fn clear(&mut self) {
-        self.vec.clear();
-        self.len = 0;
-    }
-
-    /// Returns the number of `Some(Publish)` items in the bucket list.
+    /// Returns the number of `Some` values in the vector.
     pub fn len(&self) -> usize {
-        self.len
+        self.vec.iter().filter(|x| x.is_some()).count()
     }
 
-    /// Inserts `value` into the bucket list returning the previous value, unless the `pkid` is out
-    /// of bounds.
-    ///
-    /// Returns `Err(OutOfBounds)` in case the `pkid` of the Publish is out of bounds.
+    /// Inserts a `Publish` value into the vector at the position specified by its packet identifier.
+    /// If the position is out of bounds, it returns an `OutOfBounds` error.
     pub fn insert(&mut self, value: Publish) -> Result<Option<Publish>, OutOfBounds> {
         let index = value.pkid as usize;
-        if let Some(elem) = self.vec.get_mut(index) {
-            let old = elem.replace(value);
-            if old.is_none() {
-                self.len += 1;
-            }
-            Ok(old)
-        } else {
-            Err(OutOfBounds(value.pkid))
+        match self.vec.get_mut(index) {
+            Some(slot) => Ok(slot.replace(value)),
+            None => Err(OutOfBounds(value.pkid)),
         }
     }
 
-    /// Removes the bucket at index `pkid` returning it's value.
-    ///
-    /// Returns `Err(OutOfBounds)` if `pkid` is out of bounds.
+    /// Removes a `Publish` value from the vector at the specified position.
+    /// If the position is out of bounds, it returns an `OutOfBounds` error.
     pub fn remove(&mut self, pkid: u16) -> Result<Option<Publish>, OutOfBounds> {
-        if let Some(elem) = self.vec.get_mut(pkid as usize) {
-            let old = elem.take();
-            if old.is_some() {
-                self.len -= 1;
-            }
-            Ok(old)
-        } else {
-            Err(OutOfBounds(pkid))
+        let index = pkid as usize;
+        match self.vec.get_mut(index) {
+            Some(slot) => Ok(slot.take()),
+            None => Err(OutOfBounds(pkid)),
         }
     }
 
-    /// Returns a reference to the Publish paket with `pkid`.
-    ///
-    /// Returns `Err(OutOfBounds)` if `pkid` is out of bounds.
+    /// Returns a reference to the `Publish` value at the specified position.
+    /// If the position is out of bounds, it returns an `OutOfBounds` error.
     pub fn get(&self, pkid: u16) -> Result<Option<&Publish>, OutOfBounds> {
         self.vec
             .get(pkid as usize)
             .ok_or(OutOfBounds(pkid))
             .map(Option::as_ref)
     }
-}
 
-impl From<Vec<Option<Publish>>> for OutgoingPublishBucket {
-    fn from(vec: Vec<Option<Publish>>) -> Self {
-        let len = vec.iter().filter(|v| v.is_some()).count();
-        Self { vec, len }
+    /// Drains the `Publish` values into another vector, transforming them using a specified function.
+    /// The transformation starts from the position specified by `last_puback`.
+    pub fn drain_into<T>(&mut self, vec: &mut Vec<T>, last_puback: usize, map: fn(Publish) -> T) {
+        let mut index = 0;
+        self.vec.iter_mut().for_each(|opt_publish| {
+            if let Some(publish) = opt_publish.take() {
+                if index >= last_puback {
+                    vec.push(map(publish));
+                }
+            }
+            index += 1;
+        });
+        self.vec.clear();
     }
 }
 
-/// A `set` of `pkid`'s.
-///
-/// Uses a bitset to determine if a `pkid` is in use or not.
+impl From<Vec<Option<Publish>>> for OutgoingPublishBucket {
+    /// Converts a vector of `Option<Publish>` values into an `OutgoingPublishBucket`.
+    fn from(vec: Vec<Option<Publish>>) -> Self {
+        Self { vec }
+    }
+}
+
+/// `PkidSet` is a struct that represents a set of packet identifiers (pkids).
+/// It uses a `FixedBitSet` to efficiently store the pkids.
 #[derive(Debug, Clone)]
 pub struct PkidSet {
-    set: fixedbitset::FixedBitSet,
-    len: usize,
+    set: FixedBitSet,
 }
 
 impl PkidSet {
+    /// Creates a new `PkidSet` with a specified limit.
+    /// The limit is used to initialize the `FixedBitSet` with a certain capacity.
+    pub fn with_limit(max_pkid: u16) -> Self {
+        Self {
+            set: FixedBitSet::with_capacity(max_pkid as usize + 1),
+        }
+    }
+
+    /// Creates a new `PkidSet` that can store the full range of `u16` values.
     pub fn full_range() -> Self {
         Self::with_limit(u16::MAX)
     }
 
-    pub fn with_limit(max_pkid: u16) -> Self {
-        Self {
-            set: fixedbitset::FixedBitSet::with_capacity(max_pkid as usize + 1),
-            len: 0,
-        }
-    }
-
-    /// Returns the number of `pkid`'s in the set.
+    /// Returns the number of ones (i.e., set bits) in the `FixedBitSet`.
     pub fn len(&self) -> usize {
-        self.len
+        self.set.count_ones(..)
     }
 
+    /// Inserts a pkid into the `FixedBitSet`.
+    /// If the pkid is out of bounds, it returns an `OutOfBounds` error.
+    pub fn insert(&mut self, pkid: u16) -> Result<bool, OutOfBounds> {
+        let index = pkid as usize;
+        if index >= self.set.len() {
+            return Err(OutOfBounds(pkid));
+        }
+        let was_present = self.set.put(index);
+        Ok(was_present)
+    }
+
+    /// Checks if a pkid is in the `FixedBitSet`.
+    /// This method is only available in test configurations.
+    #[cfg(test)]
+    pub fn contains(&self, pkid: u16) -> bool {
+        self.set.contains(pkid as usize)
+    }
+
+    /// Removes a pkid from the `FixedBitSet`.
+    /// If the pkid is out of bounds, it returns an `OutOfBounds` error.
+    pub fn remove(&mut self, pkid: u16) -> Result<bool, OutOfBounds> {
+        let index = pkid as usize;
+        if index >= self.set.len() {
+            return Err(OutOfBounds(pkid));
+        }
+        let was_present = self.set.contains(index);
+        self.set.set(index, false);
+        Ok(was_present)
+    }
+
+    /// Drains the pkids into another vector, transforming them using a specified function.
     pub fn drain_into<T>(&mut self, vec: &mut Vec<T>, map: impl Fn(u16) -> T) {
         for pkid in self.set.ones() {
             vec.push(map(pkid as u16));
@@ -127,43 +144,8 @@ impl PkidSet {
         self.clear();
     }
 
-    /// Empties the bitset.
+    /// Clears the `FixedBitSet`, removing all pkids.
     pub fn clear(&mut self) {
         self.set.clear();
-        self.len = 0;
-    }
-
-    /// Inserts `pkid` into the set returning the previous value.
-    pub fn insert(&mut self, pkid: u16) -> Result<bool, OutOfBounds> {
-        let index = pkid as usize;
-        if index < self.set.len() {
-            let old = self.set.put(index);
-            if !old {
-                self.len += 1;
-            }
-            Ok(old)
-        } else {
-            Err(OutOfBounds(pkid))
-        }
-    }
-
-    #[cfg(test)]
-    pub fn contains(&self, pkid: u16) -> bool {
-        self.set.contains(pkid as usize)
-    }
-
-    /// Removes `pkid` from the set returning the value or `Err(OutOfBounds)`.
-    pub fn remove(&mut self, pkid: u16) -> Result<bool, OutOfBounds> {
-        let index = pkid as usize;
-        if index < self.set.len() {
-            let old = self.set.contains(index);
-            self.set.set(index, false);
-            if old {
-                self.len -= 1;
-            }
-            Ok(old)
-        } else {
-            Err(OutOfBounds(pkid))
-        }
     }
 }
